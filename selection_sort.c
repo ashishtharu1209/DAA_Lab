c576573cd462b69dccc4d86c3390d4e5a1aa9600
ashishtharu1209 <ashishk077@academiacollege.edu.np>
2023-08-11 11:04:13 +0545

    all codes

---
+++ selection_sort.c
@@ -0,0 +1,22 @@
+//selection sort
+#include <stdio.h>
+int main() {
+   int arr[6]={7,4,10,8,3,1};
+   int n=6;
+   int i, j, position, swap;
+   for (i = 0; i < (n - 1); i++) {
+      position = i;
+      for (j = i + 1; j < n; j++) {
+         if (arr[position] > arr[j])
+            position = j;
+      }
+      if (position != i) {
+         swap = arr[i];
+         arr[i] = arr[position];
+         arr[position] = swap;
+      }
+   }
+   for (i = 0; i < n; i++)
+      printf("%d\t", arr[i]);
+   return 0;
+}
